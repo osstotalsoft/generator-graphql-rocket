e0e7408864e2aa59bfc6f6cec95bd1d18c901c10
fraliv13 <5892139+fraliv13@users.noreply.github.com>
2022-01-25 20:19:19 +0200

    DB Connection Updates

--- generators/app/templates/infrastructure/src/db/dbInstanceFactory.js
+++ generators/app/templates/infrastructure/src/db/dbInstanceFactory.js
@@ -2,70 +2,72 @@ const Knex = require("knex")
 const knexTinyLogger = require("knex-tiny-logger").default
 <%_ if(addTracing){ _%>
 const useKnexTracer = require("../tracing/knexTracer")
-const { JAEGER_DISABLED } = process.env;
+const { JAEGER_DISABLED } = process.env
 <%_}_%>
 const { generateKnexConfig } = require('./dbConfigService')
 const { initializeTarnLogging } = require("./logging/tarnLogging")
 const { Mutex } = require('async-mutex')
 
-const mutex = new Mutex();
-const { KNEX_LOGGING, KNEX_DEBUG } = process.env;
+const mutex = new Mutex()
+const { KNEX_LOGGING, KNEX_DEBUG } = process.env
 
 const logger = (_format, duration, query) => {
-    console.log(`${duration} ${query}`)
+  console.log(`${duration} ${query}`)
 }
 
 let cachedDbInstance
 const dbInstanceFactory = async () => {
-    return await dbInstanceGetOrAdd(() => {
-        const {
-            DB_HOST: server,
-            DB_PORT: port,
-            DB_USER: userId,
-            DB_PASSWORD: password,
-            DB_DATABASE: database
-        } = process.env;
-        const dbConfig = generateKnexConfig({ server, port, userId, password, database })
-        let dbInstance = new Knex(dbConfig)
-        
-        if (!dbInstance) {
-            throw new TypeError("Could not create dbInstance. Check the database configuration info and restart the server.")
-        }
+  return await dbInstanceGetOrAdd(() => {
+    const {
+      DB_HOST: server,
+      DB_PORT: port,
+      DB_USER: userId,
+      DB_PASSWORD: password,
+      DB_DATABASE: database,
+      DB_INSTANCE_NAME: instanceName,
+      DB_TRUST_SERVER_CERTIFICATE: trustServerCertificate
+    } = process.env
+    const dbConfig = generateKnexConfig({ server, port, userId, password, database, instanceName, trustServerCertificate })
+    let dbInstance = new Knex(dbConfig)
 
-        if (JSON.parse(KNEX_DEBUG)) {
-            initializeTarnLogging(dbInstance.client.pool)
-        }
+    if (!dbInstance) {
+        throw new TypeError("Could not create dbInstance. Check the database configuration info and restart the server.")
+    }
 
-        if (JSON.parse(KNEX_LOGGING)) {
-            knexTinyLogger(dbInstance, { logger });
-        }
-        <%_ if(addTracing){ _%>
-        if (!JSON.parse(JAEGER_DISABLED)) {
-            useKnexTracer(dbInstance)
-        }
-        <%_}_%>
-        return dbInstance
-    })
+    if (JSON.parse(KNEX_DEBUG)) {
+        initializeTarnLogging(dbInstance.client.pool)
+    }
+
+    if (JSON.parse(KNEX_LOGGING)) {
+        knexTinyLogger(dbInstance, { logger })
+    }
+    <%_ if(addTracing){ _%>
+    if (!JSON.parse(JAEGER_DISABLED)) {
+        useKnexTracer(dbInstance)
+    }
+    <%_}_%>
+    return dbInstance
+  })
 }
 
 async function dbInstanceGetOrAdd(factory) {
+  if (cachedDbInstance) {
+    return cachedDbInstance
+  }
+
+  const release = await mutex.acquire()
+  try {
     if (cachedDbInstance) {
-        return cachedDbInstance
+      return cachedDbInstance
     }
 
-    const release = await mutex.acquire();
-    try {
-        if (cachedDbInstance) {
-            return cachedDbInstance
-        }
+    cachedDbInstance = factory()
 
-        cachedDbInstance = factory()
-      
-        return cachedDbInstance
-    }
-    finally {
-        release();
-    }
+    return cachedDbInstance
+  }
+  finally {
+    release()
+  }
 }
 
 module.exports = dbInstanceFactory
